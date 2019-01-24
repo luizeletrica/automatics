7114caf7d46dce3789efb47122e3c3f6ff69020c
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-01-24 17:22:53 -0200

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -1,6 +1,6 @@
 function AlteraRele1(){
     var estado = document.getElementById("estado").innerHTML;
-    if((estado === "0")&&(monitora_pino_rele1 === "0")){
+    if((estado === "0")&&(monitora_pino_rele1 === "1"){
         document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
 		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
